dd55a0a05af9deba58d0182f33804f82cd574c71
zhaopeng <zhaopeng@ascs.tech>
2018-01-18 16:19:11 +0800

    feat:添加注册模块

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.set('view engine', 'ejs')
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')))
 // session 中间件
+
 app.use(session({
   name: config.session.key, // 设置 cookie 中保存 session id 的字段名称
   secret: config.session.secret, // 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
@@ -34,6 +35,12 @@ app.use(session({
 }))
 // flash 中间件，用来显示通知
 app.use(flash())
+
+// 处理表单及文件上传的中间件
+app.use(require('express-formidable')({
+  uploadDir: path.join(__dirname, 'public/img'), // 上传文件目录
+  keepExtensions: true// 保留后缀
+}))
 
 // 设置模板全局常量
 app.locals.blog = {
